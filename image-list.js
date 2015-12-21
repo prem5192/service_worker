474ad0f77ff908b504724be4044caa24eb7e81d6
premkumar_r <premkumar.rajakumar@photoninfotech.net>
2015-12-21 15:03:14 +0530

    commiting to gitpage

---
+++ image-list.js
@@ -0,0 +1,41 @@
+var Path = 'gallery/';
+
+var Gallery = { 'images' : [
+      
+  
+  {
+    'name'  : 'Banquet Frozen Salisbury Steak',
+    'alt' : 'Frozen foods',
+    'url': 'gallery/salisbury-steak.jpg',
+  },
+
+   {
+    'name'  : 'M&M Chocolate Candies Chocolate',
+    'alt' : 'Candy & Gum',
+    'url': 'gallery/m&m.jpg',
+  },
+  
+  {
+    'name'  : 'Kit Kat Chocolate',
+    'alt' : 'Candy & Gum',
+    'url': 'gallery/kit-kat.jpg',
+  },
+
+  {
+    'name'  : 'Nabisco Crackers',
+    'alt' : 'Crackers',
+    'url': 'gallery/nabisco-crackers.jpg',
+  },
+
+   {
+    'name'  : 'Hersheys Chocolate',
+    'alt' : 'Candy & Gum',
+    'url': 'gallery/hersheys.jpg',
+  },
+
+  {
+    'name'  : 'Junior Mints Chocolate',
+    'alt' : 'Candy & Gum',
+    'url': 'gallery/junior_mints.jpg',
+  }
+]};
---
+++ sw.js
@@ -0,0 +1,42 @@
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/service_worker/',
+        '/service_worker/index.html',
+        '/service_worker/style.css',
+        '/service_worker/bootstrap.min.css',
+        '/service_worker/bootstrap.min.js',
+        '/service_worker/jquery-2.1.4.min.js',
+        '/service_worker/app.js',
+        '/service_worker/image-list.js',
+        '/service_worker/foodzone_logo.jpg',
+        '/service_worker/gallery/',
+        '/service_worker/gallery/kit-kat.jpg',
+        '/service_worker/gallery/hersheys.jpg',
+        '/service_worker/gallery/junior_mints.jpg',
+        '/service_worker/gallery/m&m.jpg',
+        '/service_worker/gallery/nabisco-crackers.jpg',
+        '/service_worker/gallery/salisbury-steak.jpg',
+        
+      ]);
+    })
+  );
+});
+
+this.addEventListener('fetch', function(event) {
+  var response;
+  event.respondWith(caches.match(event.request).catch(function() {
+    return fetch(event.request);
+  }).then(function(r) {
+    response = r;
+    caches.open('v1').then(function(cache) {
+      cache.put(event.request, response);
+    });
+    return response.clone();
+  }).catch(function() {
+    return caches.match('/service_worker/gallery/kitkat.jpg');
+  }));
+});
+
+
