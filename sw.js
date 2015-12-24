075b3475f7932e2e0f278683ebaa7df7b20e2f6e
prem5192 <inbox.premkumar@gmail.com>
2015-12-24 22:41:01 +0530

    made changes

--- sw.js
+++ sw.js
@@ -12,13 +12,36 @@ this.addEventListener('install', function(event) {
         '/service_worker/image-list.js',
         '/service_worker/foodzone_logo.jpg',
         '/service_worker/gallery/',
-        '/service_worker/gallery/kit-kat.jpg'
+        '/service_worker/gallery/kit-kat.jpg',
+        '/service_worker/gallery/hersheys.jpg',
+        '/service_worker/gallery/junior_mints.jpg',
+        '/service_worker/gallery/m&m.jpg',
+        '/service_worker/gallery/nabisco-crackers.jpg',
+        '/service_worker/gallery/salisbury-steak.jpg'
       ]);
     })
   );
 });
 
 this.addEventListener('fetch', function(event) {
+  var response;
+  event.respondWith(caches.match(event.request).catch(function() {
+    console.log("from network"::::::::::");
+    return fetch(event.request);
+  }).then(function(r) {
+    response = r;
+    caches.open('v1').then(function(cache) {
+      cache.put(event.request, response);
+    });
+    console.log("from network................");
+    return response.clone();
+  }).catch(function() {
+    console.log("from cache");
+    return caches.match('/service_worker/gallery/kitkat.jpg');
+  }));
+});
+
+/*this.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.open('v1').then(function(cache) {
       return cache.match(event.request).then(function (response) {
@@ -32,4 +55,4 @@ this.addEventListener('fetch', function(event) {
     })
   );
 });
-
+*/
